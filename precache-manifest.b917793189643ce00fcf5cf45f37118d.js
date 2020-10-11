31fc00d48f4f2288ad7d550c99180ad0f6a827eb
Yesha <patelyesha356@gmail.com>
2020-10-11 16:52:01 +0530

    Updates

---
+++ precache-manifest.b917793189643ce00fcf5cf45f37118d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c919606ad3e2ab5871a8f0078984fa7b",
+    "url": "/hidden-artists/index.html"
+  },
+  {
+    "revision": "3c00cf60221195e8b73e",
+    "url": "/hidden-artists/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48dfdd09773f3a56af7b",
+    "url": "/hidden-artists/static/js/2.6148efec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hidden-artists/static/js/2.6148efec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c00cf60221195e8b73e",
+    "url": "/hidden-artists/static/js/main.32a8700b.chunk.js"
+  },
+  {
+    "revision": "3bcc7d1a5dc7b9c1a28d",
+    "url": "/hidden-artists/static/js/runtime-main.af032415.js"
+  }
+]);
