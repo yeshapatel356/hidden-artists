bd26f2d32ae75a689e0d54ef163b32bbfa818514
Yesha <patelyesha356@gmail.com>
2020-10-11 16:48:28 +0530

    Add github pages config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/Hiddenartist5"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hidden Artists
         </a>
       </header>
     </div>
