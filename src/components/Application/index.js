32f880e974e9042e489b1ab058b6031eec7d8371
yesha <patelyesha356@gmail.com>
2021-03-12 21:46:26 +0530

    ADDED PORTFOLIO, CONTACTUS, ABOUTUS

--- src/components/Application/index.js
+++ src/components/Application/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Toggle } from "../../containers/darkmode";
 import Followus from "../Followus";
 import TabBar from "../TabBar";
 
 function Application() {
   return (
     <div>
-      <Toggle />
       <TabBar />
       <Followus />
     </div>
